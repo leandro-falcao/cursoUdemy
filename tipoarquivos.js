eca1689db22ab7e3d280207d76547aff0febaf1c
leandro-falcao <leandrofalcaoti@gmail.com>
2018-09-30 19:29:15 -0300

    updae

--- tipoarquivos.js
+++ tipoarquivos.js
@@ -1,15 +1,15 @@
    const tipoNumero = 122;
    const tipoString = "tipo string";
    const tipoObjeto = { numerox: 123,
-      casa: 12345678, rua: "rua xyz"
+      casa: 12345678, rua: "xyz"
    };
    
    const tipoArray = [12, 'rua tal', "anos", tipoNumero]
    
-   console.log(tipoArray[1]);
+  // console.log(tipoArray[1]);
    
-   const dadosArray =[ [tipoArray[2] + " " + tipoArray[3]], tipoObjeto.casa, tipoObjeto.rua ];
+   const dadosArray =[ [tipoArray[2] + " " + tipoArray[3] ], tipoObjeto.casa, tipoObjeto.rua ];
    
-   console.log(dadosArray);
+//   console.log(dadosArray);
    
-   console.log()
+console.log('rua: ' +dadosArray[2] +' numero: ' + dadosArray[1])
