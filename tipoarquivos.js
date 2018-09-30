80dd3e0ea631cb533e19445f5ea740da56f650cc
leandro-falcao <leandrofalcaoti@gmail.com>
2018-09-30 19:10:33 -0300

    update

--- tipoarquivos.js
+++ tipoarquivos.js
@@ -1,1 +1,14 @@
-
+   const tipoNumero = 122;
+   const tipoString = "tipo string";
+   const tipoObjeto = { numerox: 123,
+      casa: 123, rua: "rua xyz"
+   };
+   
+   const tipoArray = [12, 'rua tal', "anos", tipoNumero]
+   
+   console.log(tipoArray[1]);
+   
+   const dadosArray = tipoArray[2] + " " + tipoArray[3];
+   
+   console.log(dadosArray);
+   
