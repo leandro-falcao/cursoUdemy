8fad83d2be127701c69d76f98211041c0c3f63ad
leandro falcao <leandrofalcaoti@gmail.com>
2018-10-03 15:39:03 -0300

    up upgrade

---
+++ index.js
@@ -0,0 +1,13 @@
+const free = 12;
+let free2 = 13;
+var free3 = 14;
+function anon(){
+
+}
+
+console.log( (free3 * free2));
+
+console.log("ola");
+console.log("ol");
+console.log("ola res");
+console.log("ow");
